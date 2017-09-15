d3c479cb1aeee8909a20f73a0330137f4a0b4178
Dharma Kshetri <dharma.kshetri@gmail.com>
2017-09-15 14:18:26 -0500

    update iframes

--- app/src/main/java/co/prandroid/soccerlivehd/Urls.kt
+++ app/src/main/java/co/prandroid/soccerlivehd/Urls.kt
@@ -7,8 +7,7 @@ class Urls {
     companion object {
         val YOUTUBE_URL="<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/kJQP7kiw5Fk\" frameborder=\"0\" allowfullscreen></iframe>"
         val SOCCER_URL="<iframe src=\"http://www.Genti.Stream/hd/hd1.php\" width=\"320\" height=\"400\" frameborder=\"0\" scrolling=\"no\" allowfullscreen</iframe>"
-        val CRICKET_URL="<script type='text/javascript'>id='willow'; id_width=620; id_height=490;</script><script type='text/javascript' src='http://cdn15.crichd.pk/channel.js'></script>"
+        val CRICKET_URL="<iframe frameborder=\"0\" marginheight=\"0\" marginwidth=\"0\" height=\"490\" src=\"http://cricbox.net/embed/star3img.php\" name=\"iframe_a\" scrolling=\"no\" width=\"620\" frameborder=\"0\" allowfullscreen>Your Browser Do not Support Iframe</iframe>"
         val GRAPH_URL= "<iframe width=\"450\" height=\"260\" style=\"border: 1px solid #cccccc;\" src=\"http://api.thingspeak.com/channels/31592/charts/1?width=450&height=260&results=60&dynamic=true\" ></iframe>"
-
     }
 }
